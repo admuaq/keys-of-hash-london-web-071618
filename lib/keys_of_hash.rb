29832c410387940a2183227ee305ee0d4ce83199
Adham Muhammad <admuaq@gmail.com>
2018-07-14 06:10:43 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(arguments)
     # code goes here
     array=[]
-    arguments.each_with_index do |string, index|
+    arguments.each do |string, index|
       self.each do |animal, area|
         if location == arguments[index]
           array << species
