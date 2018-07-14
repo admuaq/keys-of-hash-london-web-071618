b1038ee4e6558223082d9334a686e64eda8a5899
Adham Muhammad <admuaq@gmail.com>
2018-07-14 06:29:48 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
     # code goes here
     array=[]
       
-      self.for_each_value do |arr|
+      self.each_value do |arr|
         if arr.include?(arguments)
         array << arr[0] 
         end 
