14f2628283d3c6d66d71b5b833eb25352a50980a
Adham Muhammad <admuaq@gmail.com>
2018-07-14 06:06:34 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,26 +1,6 @@
 class Hash
   def keys_of(arguments)
     # code goes here
-    array = [:a]
     
-    if arguments == "Panama"
-      array = []
-      array << "red-footed tortoise"
-      array
-    elsif arguments == "Madagascar"
-      array = []
-      array << "aye-aye"
-      array << "tomato frog"
-      array
-    elsif arguments == "Australia"
-      array = []
-      array << "sugar glider"
-      array << "kangaroo"
-      array << "koala"
-      array
-    else
-      "Nil"
-    end 
-    array 
   end
 end
