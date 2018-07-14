bb2a173693b25ff33208b2c3405dd2f833e57b8c
Adham Muhammad <admuaq@gmail.com>
2018-07-14 06:12:17 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,12 +3,7 @@ class Hash
     # code goes here
     array=[]
     arguments.each do |string, index|
-      self.each do |animal, area|
-        if location == arguments[index]
-          array << species
-        end
-      end
-    end
+      
     array
   end
 end
